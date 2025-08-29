40a8df88e93d9b37b968fab9008f7ec3a5fc0762
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-08-29 19:24:22 +0300

    solve 13. Roman To Integer problem (LeetCode)

---
+++ src/RomanToInteger.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int currentVal = map.get(s.charAt(i));
+
+            if (i + 1 < s.length() && currentVal < map.get(s.charAt(i + 1))) {
+                result -= currentVal;
+            } else {
+                result += currentVal;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("III"));      // 3
+        System.out.println(romanToInt("IV"));       // 4
+        System.out.println(romanToInt("IX"));       // 9
+        System.out.println(romanToInt("LVIII"));    // 58
+        System.out.println(romanToInt("MCMXCIV"));  // 1994
+
+    }
+}
